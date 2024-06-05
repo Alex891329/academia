97759a91d5b705da12f0dc0c8ceb2c251c1b546d
Alex <alex.araujo.sj@gmail.com>
2024-06-04 21:42:48 -0300

    Adiciona tabela de alunos no Dashboard e melhorias no layout

--- minha-academia/src/componentes/Dashboard.js
+++ minha-academia/src/componentes/Dashboard.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import './Dashboard.css'; // Importe o arquivo CSS
+
 
 function Dashboard() {
   const handleLogout = () => {
@@ -13,78 +11,25 @@ function Dashboard() {
   };
 
   return (
-    <Container className="p-3">
-      <Row className="mb-4">
-        <Col>
-          <Card className="text-center" style={{ backgroundColor: 'white', border: '3px solid red' }}>
-            <Card.Body>
-              <h1>Bem-vindo ao Dashboard</h1>
-              <Card.Text>Gerenciando sua academia com eficiência e simplicidade.</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+    <div className="container">
+      <Card style={{width: '30rem', backgroundColor: 'white', border: '3px solid red' }}>
+        <Card.Body>
+          <h1>Bem-vindo ao Dashboard</h1>
+          <Card.Text>
+            {/* Conteúdo do Dashboard aqui */}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end', height: '100vh' }}>
+  <Link to="/">
+    <Button variant="danger" onClick={handleLogout}>
+      Sair
+    </Button>
+  </Link>
+</div>
 
-      <Row className="mb-4">
-        <Col md={6}>
-          <Card className="text-center" style={{ backgroundColor: 'white', border: '3px solid red' }}>
-            <Card.Body>
-              <Card.Title>Total de Alunos Cadastrados</Card.Title>
-              <Card.Text>150</Card.Text> {/* Exemplo de dado */}
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={6}>
-          <Card className="text-center" style={{ backgroundColor: 'white', border: '3px solid red' }}>
-            <Card.Body>
-              <Card.Title>Matrículas em Dia</Card.Title>
-              <Card.Text>120</Card.Text> {/* Exemplo de dado */}
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-
-      <Row className="mb-4">
-        <Col md={6}>
-          <Card className="text-center" style={{ backgroundColor: 'white', border: '3px solid red' }}>
-            <Card.Body>
-              <Card.Title>Aulas Agendadas para Hoje</Card.Title>
-              <Card.Text>10</Card.Text> {/* Exemplo de dado */}
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={6}>
-          <Card className="text-center" style={{ backgroundColor: 'white', border: '3px solid red' }}>
-            <Card.Body>
-              <Card.Title>Média de Altura dos Alunos</Card.Title>
-              <Card.Text>1.75m</Card.Text> {/* Exemplo de dado */}
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-
-      <Row className="mb-4">
-        <Col md={6}>
-          <Card className="text-center" style={{ backgroundColor: 'white', border: '3px solid red' }}>
-            <Card.Body>
-              <Card.Title>Média de Peso dos Alunos</Card.Title>
-              <Card.Text>70kg</Card.Text> {/* Exemplo de dado */}
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh' }}>
-        <Link to="/">
-          <Button variant="danger" onClick={handleLogout}>
-            Sair
-          </Button>
-        </Link>
-      </div>
-    </Container>
+    </div>
   );
 }
 
 export default Dashboard;
-
-
--- minha-academia/src/componentes/Login.js
+++ minha-academia/src/componentes/Login.js
@@ -59,3 +59,4 @@ function Login() {
 }
 
 export default Login;
+
