a85fb4fe92e26cc88b8b9eb9e03a510ea6861bee
Alex <alex.araujo.sj@gmail.com>
2024-06-06 16:14:38 -0300

    servidor backend e frontend funcionando

--- backend/server.js
+++ backend/server.js
@@ -8,16 +8,16 @@ app.use(cors());
 app.use(express.json());
 
 // Conectando ao MongoDB
-mongoose.connect('mongodb://localhost:27017/')
+mongoose.connect('mongodb://localhost:27017/', )
   .then(() => console.log('Conectado ao MongoDB'))
   .catch((err) => console.error('Erro ao conectar ao MongoDB', err));
 
-
-// Definindo o modelo de aluno
+//Definindo modelo de aluno
 const alunoSchema = new mongoose.Schema({
   nome: String,
-  email: String,
-  senha: String,
+  peso: String,
+  altura: String,
+  inicio: String
 });
 
 const Aluno = mongoose.model('Aluno', alunoSchema);
@@ -31,14 +31,13 @@ app.get('/alunos', async (req, res) => {
 // Rota para adicionar um novo aluno
 app.post('/alunos', async (req, res) => {
   try {
-    const { nome, email, senha } = req.body;
-    
-    const hashSenha = await bcrypt.hash(senha, 10);
+    const { nome, altura, peso, inicio } = req.body;
 
     const aluno = new Aluno({
       nome,
-      email,
-      senha: hashSenha,
+      altura,
+      peso,
+      inicio
     });
 
     const resultado = await aluno.save();
@@ -49,18 +48,67 @@ app.post('/alunos', async (req, res) => {
   }
 });
 
-// Rota de autenticação
+//Definindo metodo delete aluno
+app.delete('/alunos/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Aluno.findByIdAndDelete(id);
+    res.send('Aluno excluído com sucesso');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro ao excluir aluno');
+  }
+});
+
+
+  // Definindo o modelo de administrador
+const adminSchema = new mongoose.Schema({
+  nome: String,
+  email: String,
+  senha: String,
+});
+
+const Admin = mongoose.model('Admin', adminSchema);
+
+// Rota para obter todos os administradores
+app.get('/admins', async (req, res) => {
+  const admins = await Admin.find();
+  res.send(admins);
+});
+
+// Rota para adicionar um novo administrador
+app.post('/admins', async (req, res) => {
+  try {
+    const { nome, email, senha } = req.body;
+    
+    const hashSenha = await bcrypt.hash(senha, 10);
+
+    const admin = new Admin({
+      nome,
+      email,
+      senha: hashSenha,
+    });
+
+    const resultado = await admin.save();
+    res.send(resultado);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro ao adicionar administrador');
+  }
+});
+
+// Rota de autenticação para administradores
 app.post('/login', async (req, res) => {
   try {
     const { email, senha } = req.body;
 
-    const aluno = await Aluno.findOne({ email });
+    const admin = await Admin.findOne({ email });
 
-    if (!aluno) {
+    if (!admin) {
       return res.status(400).send('Usuário não encontrado');
     }
 
-    const senhaValida = await bcrypt.compare(senha, aluno.senha);
+    const senhaValida = await bcrypt.compare(senha, admin.senha);
 
     if (!senhaValida) {
       return res.status(401).send('Senha incorreta');
--- minha-academia/src/componentes/Dashboard.js
+++ minha-academia/src/componentes/Dashboard.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import './Dashboard.css'; // Importe o arquivo CSS
 
-
 function Dashboard() {
+  const [alunos, setAlunos] = useState([]);
+  const [alunoSelecionado, setAlunoSelecionado] = useState('');
+  const [alunoInfo, setAlunoInfo] = useState(null);
+
+  useEffect(() => {
+    fetchAlunos();
+  }, []);
+
+  const fetchAlunos = async () => {
+    const response = await fetch('http://localhost:3000/alunos');
+    const data = await response.json();
+    setAlunos(data);
+  };
+
   const handleLogout = () => {
     // Implemente aqui a lógica para fazer logout
+  };
+
+  const [alunoTemp, setAlunoTemp] = useState(null);
+
+  const handleAlunoChange = (event) => {
+    const id = event.target.value;
+    setAlunoSelecionado(id);
+    const alunoSelecionado = alunos.find(aluno => aluno._id === id);
+    setAlunoTemp(alunoSelecionado);
+  };
+  
+  const handleDeleteAluno = async (id) => {
+    try {
+      await fetch(`http://localhost:3000/alunos/${id}`, {
+        method: 'DELETE',
+      });
+      alert('Aluno excluído com sucesso');
+      fetchAlunos(); // Atualiza a lista de alunos após a exclusão
+    } catch (error) {
+      console.error(error);
+      alert('Erro ao excluir aluno');
+    }
   };
 
   return (
@@ -16,18 +51,34 @@ function Dashboard() {
         <Card.Body>
           <h1>Bem-vindo ao Dashboard</h1>
           <Card.Text>
-            {/* Conteúdo do Dashboard aqui */}
+            <select value={alunoSelecionado} onChange={handleAlunoChange}>
+              <option value="">Selecione um aluno</option>
+              {alunos.map((aluno) => (
+                <option key={aluno._id} value={aluno._id}>{aluno.nome}</option>
+              ))}
+              
+            </select>
+            <Button variant="primary" onClick={() => setAlunoInfo(alunoTemp)}>OK</Button>
+            {alunoInfo && (
+              <div>
+                <h2>Informações do Aluno</h2>
+                <p><strong>Nome:</strong> {alunoInfo.nome}</p>
+                <p><strong>Altura:</strong> {alunoInfo.altura}</p>
+                <p><strong>Peso:</strong> {alunoInfo.peso}</p>
+                <p><strong>Início:</strong> {alunoInfo.inicio}</p>
+                <Button variant="danger" onClick={() => handleDeleteAluno(alunoInfo._id)}>Excluir</Button>
+              </div>
+            )}
           </Card.Text>
         </Card.Body>
       </Card>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end', height: '100vh' }}>
-  <Link to="/">
-    <Button variant="danger" onClick={handleLogout}>
-      Sair
-    </Button>
-  </Link>
-</div>
-
+        <Link to="/">
+          <Button variant="danger" onClick={handleLogout}>
+            Sair
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
